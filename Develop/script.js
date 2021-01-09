47bf7d370f465c43a310b35d60d0fee5ab9ba022
rnick1 <rnick0260@gmail.com>
2021-01-08 17:05:02 -0800

    Created functions and played some more with the alert, prompt, and confirms.

--- Develop/script.js
+++ Develop/script.js
@@ -1,74 +1,116 @@
-var lowerCase = [
-  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-]
-
-var upperCase = [
-  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-]
-
-var numericCharacters = [
-
-"1", "2", "3", "4", "5", "6", "7", "8", "9"
-]
-
-var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.',
-];
-
-
-
-alert("Welcome to my Password Generator!");
-
-var promptLength = prompt("How many characters would you like your password to have? Note: your password must have between 7-129 characters.");
-
-if(promptLength === false) {
-  alert("You must specify a length for your password.")
+var allFunctions = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  special: getRandomSpecial
 }
 
-// if(promptLength < 8 ) {
-//   alert("Your password must contain 8 or more characters.")
-// }
+// Generator functions - http://www.net-comber.com/charset.html
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getRandomLower());
 
-// if(promptLength > 128 ) {
-//   alert("Your password must contain 128 or fewer characters.")
-// }
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getRandomUpper());
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNumber());
+
+function getRandomSpecial() {
+  var special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.',];
+  return special [Math.floor(Math.random() * special.length)];
+}
+console.log(getRandomSpecial());
+
+// var lowerCase = [
+//   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+// ]
+
+// var upperCase = [
+//   "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+// ]
+
+// var numericCharacters = [
+
+// "1", "2", "3", "4", "5", "6", "7", "8", "9"
+// ]
+
+// var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.',
+// ];
+alert("Welcome to my Password Generator!");
+
+var promptLength = prompt("How many characters would you like your password to have? Note: your password must have 8-128 characters.");
+
+
+function getPasswordOptions() {
+  var length = parseInt(prompt('How many characters would you like your password to contain?'));
+}
+
+if (isNaN(promptlength) === true) {
+   alert('Password length must be provided as a number.');
+}
+
+if (promptlength < 8 ) {
+     alert('Password length must be at least 8 characters.');
+}
+
+if (promptlength > 128) {
+     alert('Password length must be less than 128 characters');
+}
+
+
 
 var promptNumber = confirm("Would you like to have Numbers in your password?");
 
 var promptSpecialCharacters = confirm("Would you like to have Special Characters in your password?");
-
+  
 var promptLowerCase = confirm("Would you like to have Lower Case Letteres in your password?");
-
+  
 var promptUpperCase = confirm("Would you like to have Upper Case Letters in your password?")
 
-if(promptLength === true){
 
-}
+// for (var i = 0; i = 7; i++){
 
-console.log(Math.round(Math.random() * 10))
-
-
-
-// function promptNumber() {
-//   var wantsNumbers = confirm("Would you like to have Numbers in your password?");
+// var randomNumber = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+// console.log(randomNumber)
 // }
 
-// function promptSpecialCharacters() {
-//   var wantsSpecial = confirm("Would you like to have Special Characters in your password?");
-// }
 
-// function promptLowerCase() {
-//   var wantsLower = confirm("Would you like to have Lowercase Characters in your password?");
-// }
 
-// function promptUpperCase() {
-//   var wantsUpper = confirm("Would you like to have Uppercase Characters in your password?");
-// }
 
-// if(promptNumber = true) {
-//   return alert("Great! I'll add numbers to your password!");}
-// else(promptNumber - false); {
-//   return alert("Okay. I won't add any numbers this time.");
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // // Assignment Code
 // var generateBtn = document.querySelector("#generate");
