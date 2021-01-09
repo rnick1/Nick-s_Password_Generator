c148fd8b8bbccc2416a65b2f43a9e196772a8bb5
rnick1 <rnick0260@gmail.com>
2021-01-08 20:00:55 -0800

    Added new function.

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,3 @@
-var allFunctions = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  special: getRandomSpecial
-}
-
-// Generator functions - http://www.net-comber.com/charset.html
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-console.log(getRandomLower());
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-console.log(getRandomUpper());
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-console.log(getRandomNumber());
-
-function getRandomSpecial() {
-  var special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.',];
-  return special [Math.floor(Math.random() * special.length)];
-}
-console.log(getRandomSpecial());
-
 // var lowerCase = [
 //   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 // ]
@@ -62,9 +33,41 @@ var promptUpperCase = confirm("Would you like to have Upper Case Letters in your
 // }
 
 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getRandomLower());
 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getRandomUpper());
 
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNumber());
 
+function getRandomSpecial() {
+  var special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.',];
+  return special [Math.floor(Math.random() * special.length)];
+}
+console.log(getRandomSpecial());
+
+var allFunctions = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  special: getRandomSpecial
+}
+
+function getPasswordSection() {
+  return (getRandomNumber() + getRandomSpecial() + getRandomUpper() + getRandomLower());
+}
+console.log(getPasswordSection());
+
+for (var i = 0; i < 8; i++) getPasswordSection(i);
+console.log(getPasswordSection(i));
 
 
 
