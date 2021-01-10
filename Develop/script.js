35cfe63ef3bd243383310816ff65c8681ae0260d
rnick1 <rnick0260@gmail.com>
2021-01-10 13:39:04 -0800

    Completed first draft alerts and prompts. Working through functions.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,19 @@
+var lowerCase = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+];
+
+var upperCase = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+
+var numericCharacters = [
+
+"1", "2", "3", "4", "5", "6", "7", "8", "9"
+];
+
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.',
+];
+
 alert("Welcome to my Password Generator!");
 
 var passwordLength = parseInt(prompt("How many characters would you like your password to have? Note: your password must have 8-128 characters."));
@@ -21,67 +37,114 @@ if (passwordLength > 128) {
   console.log(passwordLength);
 }
 
-function getPasswordOptions() {
-
-var promptNumber = confirm("Would you like to have Numbers in your password?");
-
-var promptSpecialCharacters = confirm("Would you like to have Special Characters in your password?");
+  var promptNumber = confirm("Would you like to have Numbers in your password?");
+  var promptSpecialCharacters = confirm("Would you like to have Special Characters in your password?");
+  var promptLowerCase = confirm("Would you like to have Lower Case Letteres in your password?");
+  var promptUpperCase = confirm("Would you like to have Upper Case Letters in your password?");
   
-var promptLowerCase = confirm("Would you like to have Lower Case Letteres in your password?");
-  
-var promptUpperCase = confirm("Would you like to have Upper Case Letters in your password?");
-
 if (!promptNumber && !promptSpecialCharacters && !promptLowerCase && !promptUpperCase) {
   alert("Please select at least one character type.");
-  return getPasswordOptions;
-  // var promptNumber = confirm("Would you like to have Numbers in your password?");
-  // var promptSpecialCharacters = confirm("Would you like to have Special Characters in your password?");
-  // var promptLowerCase = confirm("Would you like to have Lower Case Letteres in your password?");
-  // var promptUpperCase = confirm("Would you like to have Upper Case Letters in your password?");
-};
+  var promptNumber = confirm("Would you like to have Numbers in your password?");
+  var promptSpecialCharacters = confirm("Would you like to have Special Characters in your password?");
+  var promptLowerCase = confirm("Would you like to have Lower Case Letteres in your password?");
+  var promptUpperCase = confirm("Would you like to have Upper Case Letters in your password?");
 };
 
-getPasswordOptions();
+var getPasswordOptions = {
+  Length: passwordLength,
+  Numbers: promptNumber,
+  Special: promptSpecialCharacters,
+  Lowercase: promptLowerCase,
+  Uppercase: promptUpperCase
+};
+
+console.log(getPasswordOptions.Numbers);
 
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-console.log(getRandomLower());
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-console.log(getRandomUpper());
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-console.log(getRandomNumber());
-
-function getRandomSpecial() {
-  var special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.',];
-  return special [Math.floor(Math.random() * special.length)];
-}
-console.log(getRandomSpecial());
-
-var allFunctions = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  special: getRandomSpecial
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+  
+  return randElement;
 }
 
-for(var i = 0; i < passwordLength; i++) {
-    function getPasswordSection() {
-      return (getRandomNumber() + getRandomSpecial() + getRandomUpper() + getRandomLower());
-    };
-    console.log(getPasswordSection());
+// function generatePassword() {
+  var options = getPasswordOptions;
+  var result = [];
+  var possibleCharacters = [];
+  var guaranteedCharacters = [];
+// }
+
+
+
+
+if(getPasswordOptions.passwordLength) {
+  possibleCharacters = possibleCharacters.concat(passwordLength)
+  guaranteedCharacters.push(getRandom(specialCharacters));
+}
+
+console.log(possibleCharacters);
+console.log(guaranteedCharacters);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getRandomLower() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+// console.log(getRandomLower());
+
+// function getRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
+// console.log(getRandomUpper());
+
+// function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
+// console.log(getRandomNumber());
+
+// function getRandomSpecial() {
+//   var special = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','(',')','{','}','[',']','~','-','_','.',];
+//   return special [Math.floor(Math.random() * special.length)];
+// }
+// console.log(getRandomSpecial());
+
+// var allFunctions = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   special: getRandomSpecial
+// }
+
+// for(var i = 0; i < passwordLength; i++) {
+//     function getPasswordSection() {
+//       return (getRandomNumber() + getRandomSpecial() + getRandomUpper() + getRandomLower());
+//     };
+//     console.log(getPasswordSection());
     
-    console.log(i);
-  };
+//     console.log(i);
+//   };
 
-  getPasswordSection();
+//   getPasswordSection();
 
 
 
